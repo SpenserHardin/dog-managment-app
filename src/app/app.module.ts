e699aebd86aba8418ea60c2f26e264ac32f3ab92
Spenser Hardin <shardin@pillartechnology.com>
2018-02-09 22:44:09 -0600

    Adding compoents and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DogsComponent } from './dogs/dogs.component';
+import { DogListComponent } from './dogs/dog-list/dog-list.component';
+import { DogItemComponent } from './dogs/dog-list/dog-item/dog-item.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DogsComponent,
+    DogListComponent,
+    DogItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/dogs/dogs.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dogs',
+  templateUrl: './dogs.component.html',
+  styleUrls: ['./dogs.component.css']
+})
+export class DogsComponent implements OnInit {
+
+  dogList = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
